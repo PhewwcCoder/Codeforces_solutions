80c70869658bf4309030c281e74b74fd9fadd94d
PhewwcCoder <op143aryan@gmail.com>
2025-10-05 02:50:30 +0600

    greedy, sortings

---
+++ 800-1000/2149B__Unconventional Pairs.py
@@ -0,0 +1,26 @@
+"""
+Problem: Unconventional Pairs
+Rating: 800
+Tags: greedy, sortings
+Link: https://codeforces.com/problemset/problem/2149/B
+"""
+
+import sys
+input = sys.stdin.readline
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    arr1 = []
+    arr2 = []
+    diff = []
+    for i in range(n):
+        if i%2 == 0:
+            arr1.append(arr[i])
+        else:
+            arr2.append(arr[i])
+    for i in range(len(arr1)):
+        diff.append(abs(arr1[i]-arr2[i]))
+    print(max(diff))
+
