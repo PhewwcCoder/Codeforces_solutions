301223f33eb5c3a9285d7d18c00eb4794b816cf7
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 06:53:17 +0600

    ✅ 2009A - Minimize!
    Rating: 800 ⭐ | Solution: 211B (11 lines)
    Link: https://codeforces.com/problemset/problem/2009/A

--- 2009A__Minimize!.py
+++ 2009A__Minimize!.py
@@ -1,3 +1,10 @@
+"""
+Problem: Minimize!
+Rating: 800
+Tags: brute force, math
+Link: https://codeforces.com/problemset/problem/2009/A
+"""
+
 t=int(input())
 for i in range(t):
     a,b=map(int, input().split())
