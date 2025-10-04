b16b722cb6c7b02ed280018a6cd5245cae7a686b
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 06:53:17 +0600

    ✅ 1A - Theatre Square
    Rating: 1000 ⭐ | Solution: 211B (11 lines)
    Link: https://codeforces.com/problemset/problem/1/A

--- 1A__Theatre.py
+++ 1A__Theatre.py
@@ -1,3 +1,10 @@
+"""
+Problem: Theatre Square
+Rating: 1000
+Tags: math
+Link: https://codeforces.com/problemset/problem/1/A
+"""
+
 import math
 m,n,a=map(int, input().split())
 count=math.ceil(m/a)*math.ceil(n/a)
