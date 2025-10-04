be6b775e2941ea9418dc4a2a473d75c870227d46
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 07:18:10 +0600

    ✅ 2086A - Cloudberry Jam
    Rating: 800 ⭐
    Link: https://codeforces.com/problemset/problem/2086/A

---
+++ 800-1000/2086A__Cloudberry Jam.py
@@ -0,0 +1,11 @@
+"""
+Problem: Cloudberry Jam
+Rating: 800
+Tags: math
+Link: https://codeforces.com/problemset/problem/2086/A
+"""
+
+t=int(input())
+for i in range(t):
+    a=int(input())
+    print(2*a)
