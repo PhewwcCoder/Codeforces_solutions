9b27d6885304bc61c34473789ebbe23da840cd59
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 06:53:18 +0600

    ✅ 200B - Drinks
    Rating: 800 ⭐ | Solution: 226B (13 lines)
    Link: https://codeforces.com/problemset/problem/200/B

--- 200B__Drinks.py
+++ 200B__Drinks.py
@@ -1,3 +1,10 @@
+"""
+Problem: Drinks
+Rating: 800
+Tags: implementation, math
+Link: https://codeforces.com/problemset/problem/200/B
+"""
+
 t=int(input())
 o=list(map(int, input().split()))
 count=0
