aa27a302d0c43fb2871a5edd8de92cf951d0c673
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 07:18:11 +0600

    ✅ 2146A - Equal Occurrences
    Rating: 800 ⭐
    Link: https://codeforces.com/problemset/problem/2146/A

---
+++ 800-1000/2146A__Equal Occurences.py
@@ -0,0 +1,25 @@
+"""
+Problem: Equal Occurrences
+Rating: 800
+Tags: brute force, greedy, implementation
+Link: https://codeforces.com/problemset/problem/2146/A
+"""
+
+import sys,operator
+from collections import Counter
+input = sys.stdin.readline
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    count = dict(Counter(arr))
+    res = dict(sorted(count.items(), key = operator.itemgetter(1)))
+    k = len(res)
+    output = 0
+    for key in res:
+        result = res[key]*k
+        k-=1
+        if result > output:
+            output = result
+    print(output)
+
