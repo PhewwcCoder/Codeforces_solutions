11a78474373461fe6bb7a198f7fa888390a661bf
PhewwcCoder <op143aryan@gmail.com>
2025-10-05 02:50:33 +0600

    implementation

---
+++ 800-1000/61A__Ultra-Fast Mathematician.py
@@ -0,0 +1,16 @@
+"""
+Problem: Ultra-Fast Mathematician
+Rating: 800
+Tags: implementation
+Link: https://codeforces.com/problemset/problem/61/A
+"""
+
+a=input()
+b=input()
+output=""
+for i in range(len(a)):
+    if a[i]!=b[i]:
+        output+="1"
+    else:
+        output+="0"
+print(output)
