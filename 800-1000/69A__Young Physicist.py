a5a7f59de5e3cac2b50b21b4e87b56a66b405020
PhewwcCoder <op143aryan@gmail.com>
2025-10-05 02:50:34 +0600

    implementation, math

---
+++ 800-1000/69A__Young Physicist.py
@@ -0,0 +1,18 @@
+"""
+Problem: Young Physicist
+Rating: 1000
+Tags: implementation, math
+Link: https://codeforces.com/problemset/problem/69/A
+"""
+
+n=int(input())
+result=[0,0,0]
+for i in range(n):
+    a=list(map(int, input().split()))
+    result[0]+=a[0]
+    result[1]+=a[1]
+    result[2]+=a[2]
+if result==[0,0,0]:
+    print("YES")
+else:
+    print("NO")
