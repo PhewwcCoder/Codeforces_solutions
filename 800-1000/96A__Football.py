8e04c104524f65a6f9438b2dad3d5fe5b126810f
PhewwcCoder <op143aryan@gmail.com>
2025-10-05 02:50:34 +0600

    implementation, strings

---
+++ 800-1000/96A__Football.py
@@ -0,0 +1,12 @@
+"""
+Problem: Football
+Rating: 900
+Tags: implementation, strings
+Link: https://codeforces.com/problemset/problem/96/A
+"""
+
+user=input()
+if "1111111" in user or "0000000" in user:
+    print("YES")
+else:
+    print("NO")
