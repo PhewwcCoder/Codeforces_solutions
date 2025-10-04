8a21a4baebf64bbccf62b140a295863765ce6d24
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 06:53:19 +0600

    ✅ 2033A - Sakurako and Kosuke
    Rating: 800 ⭐ | Solution: 297B (14 lines)
    Link: https://codeforces.com/problemset/problem/2033/A

--- 2033A__Sakurako and Kosuke.py
+++ 2033A__Sakurako and Kosuke.py
@@ -1,3 +1,10 @@
+"""
+Problem: Sakurako and Kosuke
+Rating: 800
+Tags: constructive algorithms, implementation, math
+Link: https://codeforces.com/problemset/problem/2033/A
+"""
+
 t=int(input())
 for i in range(t):
     n=int(input())
