46556f52f7bdc19f4f053c62c9a29695fed064c6
PhewwcCoder <op143aryan@gmail.com>
2025-10-05 02:50:34 +0600

    implementation

---
+++ 800-1000/705A__Hulk.py
@@ -0,0 +1,21 @@
+"""
+Problem: Hulk
+Rating: 800
+Tags: implementation
+Link: https://codeforces.com/problemset/problem/705/A
+"""
+
+s=int(input())
+output=""
+for i in range(s):
+    if i==s-1 and i%2==0:
+        output+="I hate it"
+        break
+    if i==s-1 and i%2!=0:
+        output+="I love it"
+        break
+    elif i%2==0:
+        output+="I hate that "
+    else:
+        output+="I love that "
+print(output)
