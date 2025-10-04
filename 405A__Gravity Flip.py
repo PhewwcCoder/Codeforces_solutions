c45e85ea5a1c9573336571e57bacd2dfc8cd60ae
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 06:53:24 +0600

    ✅ 405A - Gravity Flip
    Rating: 900 ⭐ | Solution: 238B (12 lines)
    Link: https://codeforces.com/problemset/problem/405/A

--- 405A__Gravity Flip.py
+++ 405A__Gravity Flip.py
@@ -1,3 +1,10 @@
+"""
+Problem: Gravity Flip
+Rating: 900
+Tags: greedy, implementation, sortings
+Link: https://codeforces.com/problemset/problem/405/A
+"""
+
 n=int(input())
 x=list(map(int, input().split()))
 x.sort()
