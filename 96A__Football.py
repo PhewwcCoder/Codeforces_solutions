006ed1df090df1731f79d8a0bcf86ecadcf72ebc
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 06:53:26 +0600

    ✅ 96A - Football
    Rating: 900 ⭐ | Solution: 227B (12 lines)
    Link: https://codeforces.com/problemset/problem/96/A

--- 96A__Football.py
+++ 96A__Football.py
@@ -1,3 +1,10 @@
+"""
+Problem: Football
+Rating: 900
+Tags: implementation, strings
+Link: https://codeforces.com/problemset/problem/96/A
+"""
+
 user=input()
 if "1111111" in user or "0000000" in user:
     print("YES")
