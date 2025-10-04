4f1eccfe2ad946b9d947f959b2c7e7d8d3b1b88a
PhewwcCoder <op143aryan@gmail.com>
2025-10-04 07:18:06 +0600

    ✅ 1992B - Angry Monk
    Rating: 800 ⭐
    Link: https://codeforces.com/problemset/problem/1992/B

---
+++ 800-1000/1992B__Angry Monk.py
@@ -0,0 +1,20 @@
+"""
+Problem: Angry Monk
+Rating: 800
+Tags: greedy, math, sortings
+Link: https://codeforces.com/problemset/problem/1992/B
+"""
+
+t=int(input())
+for _ in range(t):
+    n,n2=map(int, input().split())
+    l=input().split()
+    k=list(map(int,l))
+    k.sort()
+    count=0
+    for i in range(len(k)-1):
+        if k[i]==1:
+            count+=1
+        else:
+            count+=k[i]-1+k[i]           
+    print(count)
