1990c25c61c15398442f5a2cc157a8f7afb468c4
PhewwcCoder <op143aryan@gmail.com>
2025-10-05 02:50:24 +0600

    greedy, sortings

---
+++ 800-1000/160A__Twins.py
@@ -0,0 +1,22 @@
+"""
+Problem: Twins
+Rating: 900
+Tags: greedy, sortings
+Link: https://codeforces.com/problemset/problem/160/A
+"""
+
+n=int(input())#number of coins
+a=list(map(int, input().split()))
+a.sort()
+a.reverse()
+me=0
+count=0
+for i in a:
+    count+=i
+twin=count    
+for i in range(len(a)):
+    me+=a[i]
+    twin-=a[i]
+    if me>twin:
+        print(i+1)
+        break
